7ee87fc233ce3b4e9ec02414330df435a76be25f
koverchik <koverchik-work@yandex.ru>
2020-02-19 00:54:52 +0300

    basic layout

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<?php require_once 'header.php'?>
+  <body>
+  <header>
+		<div class="customers">
+			<h1><a href="create.php">Создать задачу</a></h1>
+		</div>
+  </header>
+  	   <main id="button_enter">
+				 <div>
+					 <h2>TODO</h2>
+				 <ul>
+					 <li class="create">
+						 <a href="#">Взять задачу</a>
+						 <div>
+								Описание задачи.
+							</div>
+							<div class="comment">
+								<img src="image/comment.png"/>
+								<p>
+									1
+								</p>
+							</div>
+					 </li>
+				 </ul>
+			</div>
+
+				<div>
+					<h2>DOING</h2>
+					<ul>
+						<li class="in_work">
+							<a href="#">Задача в работе</a>
+							<div>
+								Описание задачи.
+							</div>
+							<div class="comment">
+								<img src="image/comment.png"/>
+								<p>
+									1
+								</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+				<div>
+					<h2>DONE</h2>
+					<ul>
+						<li class="delivery">
+								<a href="#">Задача выполнена</a>
+								<div>
+									Описание задачи.
+								</div>
+								<div class="comment">
+									<img src="image/comment.png"/>
+									<p>
+										1
+									</p>
+								</div>
+						</li>
+					</ul>
+				</div>
+      </main>
+    </body >
+    </html>
