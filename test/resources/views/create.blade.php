0187a9439760f494b8d2639258fa5e24884f22e9
koverchik <koverchik-work@yandex.ru>
2020-02-19 19:02:52 +0300

    feat: pages index and create

---
+++ test/resources/views/create.blade.php
@@ -0,0 +1,26 @@
+@extends('main.head')
+
+@section('base')
+<a href="index"><img src="image/back.png" alt="back_button"/></a>
+<h1>Создать</h1>
+  <form id="form" method="get" action="presentation.php">
+    <div>
+       <label for="name">Название</label><input type="text" name="name" required placeholder="">
+    </div>
+    <div>
+        <label for="note">Описание</label><textarea type="text" size="256" name="note" form="form-presentation"></textarea><br>
+    </div>
+    <div>
+        <label for="status">Статус</label><input list="status"  name="status" form="form-deal">
+        <datalist id="status">
+          <option>TODO</option>
+          <option>DOING</option>
+          <option>DONE</option>
+      </datalist>
+    </div>
+    <div>
+      <input id="button-send" type=submit value="Записать">
+    </div>
+  </form>
+
+@endsection
--- test/webpack.mix.js
+++ test/webpack.mix.js
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    mix.styles([
+        'public/css/vendor/reset.css',
+        'public/css/vendor/style.css'
+    ], 'public/css/all.css');
