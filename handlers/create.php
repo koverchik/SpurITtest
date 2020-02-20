12f5de9a4a581c422895c47e3149a702411f8632
koverchik <koverchik-work@yandex.ru>
2020-02-20 20:06:11 +0300

    feat: watch list task

--- handlers/create.php
+++ handlers/create.php
@@ -4,21 +4,20 @@
   <header>
     <a href="../index.php"><img src="../image/back.png" alt="back_button"/></a>
 		<div class="customers">
-      <?php require_once 'connection.php'?>
       <?php
+        require_once 'login.php';
+        require_once 'connection.php'?>
+      <?php
+
       $name = $_GET["name"];
       $note = $_GET["note"];
       $status = $_GET["status"];
-
-      require_once 'login.php';
-      $conn = new mysqli($hn, $un, $pw, $db);
-      if ($conn->connect_error) die($conn->connect_error);
-
       $query = "INSERT INTO tasks(name, description, id_status) VALUES ('$name', '$note', '$status');";
       $result = $conn->query($query);
 
+
       ?>
-			<h1>Задача с названием <?php echo "$name";?> создана </h1>
+			<h1>Задача с названием &laquo;<?php echo "$name";?>&raquo; создана </h1>
 		</div>
 
 
--- index.php
+++ index.php
@@ -1,62 +1,52 @@
 <!DOCTYPE html>
 <?php require_once 'header.php'?>
+<?php require_once 'handlers/login.php'?>
+<?php require_once 'handlers/connection.php'?>
   <body>
   <header>
 		<div class="customers">
 			<h1><a href="create.php">Создать задачу</a></h1>
 		</div>
   </header>
-  	   <main id="button_enter">
-				 <div>
-					 <h2>TODO</h2>
-				 <ul>
-					 <li class="create">
-						 <a href="#">Взять задачу</a>
-						 <div>
-								Описание задачи.
-							</div>
-							<div class="comment">
-								<img src="image/comment.png"/>
-								<p>
-									1
-								</p>
-							</div>
-					 </li>
-				 </ul>
-			</div>
+  <main id="button_enter">
+    <div>
+      <h2>TODO</h2>
+       <ul>
+  <?php
+          $query = "SELECT tasks.name, tasks.description, tasks.id  FROM tasks WHERE tasks.id_status='1';";
+
+          $result = $conn->query($query);
+
+          while ($obj = $result->fetch_object()) {
+          printf ("<li class=\"create\"> <a href=\"redact.php?id=$obj->id\"> $obj->name</a> <div>$obj->description</div><div class=\"comment\">	<img src=\"image/comment.png\"/>
+            <p>1</p></div></li>");
+      }
+
+
+          ?>
+    </ul>
+</div>
 
 				<div>
 					<h2>DOING</h2>
 					<ul>
-						<li class="in_work">
-							<a href="#">Задача в работе</a>
-							<div>
-								Описание задачи.
-							</div>
-							<div class="comment">
-								<img src="image/comment.png"/>
-								<p>
-									1
-								</p>
-							</div>
-						</li>
+            <?php   $query = "SELECT tasks.name, tasks.description, tasks.id  FROM tasks WHERE tasks.id_status='2';";
+                    $result = $conn->query($query);
+                    while ($obj = $result->fetch_object()) {
+                    printf ("<li class=\"in_work\"> <a href=\"redact.php?id=$obj->id\"> $obj->name</a> <div>$obj->description</div><div class=\"comment\">	<img src=\"image/comment.png\"/>
+                      <p>1</p></div></li>");}?>
+
 					</ul>
 				</div>
 				<div>
 					<h2>DONE</h2>
 					<ul>
-						<li class="delivery">
-								<a href="#">Задача выполнена</a>
-								<div>
-									Описание задачи.
-								</div>
-								<div class="comment">
-									<img src="image/comment.png"/>
-									<p>
-										1
-									</p>
-								</div>
-						</li>
+            <?php   $query = "SELECT tasks.name, tasks.description, tasks.id  FROM tasks WHERE tasks.id_status='3';";
+                    $result = $conn->query($query);
+                    while ($obj = $result->fetch_object()) {
+                    printf ("<li class=\"delivery\"> <a href=\"redact.php?id=$obj->id\"> $obj->name</a> <div>$obj->description</div><div class=\"comment\">	<img src=\"image/comment.png\"/>
+                      <p>1</p></div></li>");}?>
+
 					</ul>
 				</div>
       </main>
